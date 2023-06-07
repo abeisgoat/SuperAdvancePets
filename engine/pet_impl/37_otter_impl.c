123cc82d0c08a5f3dd424713ba660bc0d0077121
Abe Haskins <abeisgreat@abeisgreat.com>
2023-06-07 17:20:58 -0500

    More pet

--- engine/pet_impl/37_otter_impl.c
+++ engine/pet_impl/37_otter_impl.c
@@ -3,15 +3,16 @@
 #include "../../src/animations.h"
 #include <stdio.h>
 
+// TODO This trigger based on store position, not held position
 void otterTriggerBuy(int usOrThem, PetTeam us, PetTeam them, struct Pet * selfPet, struct Pet * activatingPet, PetTeam store) {
     printf("Activated Otter trigger Buy");
     for (int l=1; l<= expToLevel(selfPet->experience); l++) {
-        struct Pet * friend1 = randomOtherTeamMember(them, selfPet);
+        struct Pet * friend1 = randomOtherTeamMember(us, selfPet);
 
-        int givePos = petPosition(usOrThem, us, them, selfPet);
+        int selfPos = petPosition(usOrThem, us, them, selfPet);
         int friend1Pos = petPosition(usOrThem, us, them, friend1);
 
-        animateStatsToTeamPosition(givePos, friend1Pos);
+        animateStatsToTeamPosition(selfPos, friend1Pos);
         resolveAnimation();
 
         friend1->attack+=1;
