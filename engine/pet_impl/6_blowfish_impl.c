52f59913181181cdee636b7bab778e282156d681
Abe <abeisgreat@abeisgreat.com>
2023-07-18 09:19:04 -0500

    Blowfish

--- engine/pet_impl/6_blowfish_impl.c
+++ engine/pet_impl/6_blowfish_impl.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void blowfishTriggerHurt(int usOrThem, PetTeam us, PetTeam them, struct Pet * selfPet, struct Pet * activatingPet, PetTeam store) {
-    printf("Activated Blowfish trigger Hurt");
+    printf("Activated Blowfish trigger Hurt\n");
 
     struct Pet * enemy = randomOtherTeamMember(them, selfPet);
 
